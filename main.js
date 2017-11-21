22dce1bba350197d4128ef7ae9e81dee62344ce6
Dan Actor <danltactor@gmail.com>
2017-11-21 16:06:59 -0700

    break string to array, eval

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-// ** create a 4x4 grid with another full bar/row at top for input
-// ** make each grid space a button
-// ** each button logs its value or operator
-// ** user can input values/operators and receive result in bar
+// ** cancatanate multiple digit numbers
 // ** dividing by zero not allowed, basic logic to prevent null input and display error message
 // *** === BONUS === *** \\\
 // ** Allow calculations to be added on to. e.g. 5 + 5 = 10 + 4 = 14
@@ -9,3 +6,19 @@
 // ** Create a memory add and memory clear functionality.
 // ** Implement as many advanced calculator features as you have time for (eg. sin, cosin, power etc...)
 // ************use .eval, save yourself
+
+var eq = ""
+
+$('button').click( function(){
+  if(this.id === "eval"){
+    eq.split(/(\+|-|\/|\*)/)
+    // console.log(eq)
+    $('.output_area').html(eval(eq))
+  } else if(this.id === "clear") {
+    eq = ""
+    $('.output_area').html("JS Calculator")
+  } else {
+    eq += this.id
+    $('.output_area').html(eq)}
+    // console.log(eq)
+});
