2f9e67f75858eb5ff1db45899564d96567644800
Dan Actor <danltactor@gmail.com>
2017-11-20 22:31:01 -0700

    push to git

---
+++ main.js
@@ -0,0 +1,10 @@
+// ** create a 4x4 grid with another full bar/row at top for input
+// ** make each grid space a button
+// ** each button logs its value or operator
+// ** user can input values/operators and receive result in bar
+// ** dividing by zero not allowed, basic logic to prevent null input and display error message
+// *** === BONUS === *** \\\
+// ** Allow calculations to be added on to. e.g. 5 + 5 = 10 + 4 = 14
+// ** Show a history of calculations
+// ** Create a memory add and memory clear functionality.
+// ** Implement as many advanced calculator features as you have time for (eg. sin, cosin, power etc...)
