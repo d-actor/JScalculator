46f92ca0fb0a7b3acde62d3d400462a46681fe87
Dan Actor <danltactor@gmail.com>
2017-11-21 18:39:32 -0700

    complete basic objectives

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// ** dividing by zero not allowed, basic logic to prevent null input and display error message
 // *** === BONUS === *** \\\
 // ** Show a history of calculations
 // ** Create a memory add and memory clear functionality.
@@ -10,8 +9,14 @@ $('button').click( function(){
   if(this.id === "eval"){
     if (eq !== ""){
       eq.split(/(\+|-|\/|\*)/)
-      $('.output_area').html(eval(eq))
-      eq = eval(eq)
+      if (eq[0] === "0" && eq[1] === "/" || eq[2] === "0" && eq[1] === "/"){
+        alert("I don't have time to explain the ins and outs right now, but that's gonna be a no-go")
+        eq = ""
+        $('.output_area').html("Infinity?")
+      } else {
+        $('.output_area').html(eval(eq))
+        eq = eval(eq)
+      };
     } else {
       alert("...nothing */+- nothing is still nothing...")
     }
